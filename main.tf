5b7c4d1d58dceb06ee9ce36207455729da867620
abdulhexa <121549928+abdulhexa@users.noreply.github.com>
2023-01-19 15:03:43 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,6 +7,30 @@ resource "azurerm_resource_group" "example" {
   location = var.location
 }
 
+resource "azurerm_container_registry" "example" {
+  name                = "${var.prefix}registry"
+  resource_group_name = "${azurerm_resource_group.example.name}"
+  location            = "${azurerm_resource_group.example.location}"
+  sku                 = "Standard"
+}
+
+resource "azurerm_kubernetes_cluster" "example" {
+  name                = "${var.prefix}-k8s"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  dns_prefix          = "${var.prefix}-k8s"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_DS2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
+
 resource "azurerm_sql_server" "example" {
   name                         = "${var.prefix}-sqlsvr"
   resource_group_name          = azurerm_resource_group.example.name
