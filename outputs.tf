1ed6dac0ef20502353db44a45904171368bdf2d6
abdulhexa <121549928+abdulhexa@users.noreply.github.com>
2023-01-19 15:05:14 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,28 @@ output "sql_server_fqdn" {
 output "database_name" {
   value = azurerm_sql_database.example.name
 }
+
+
+output "id" {
+  value = azurerm_kubernetes_cluster.example.id
+}
+
+output "kube_config" {
+  value = azurerm_kubernetes_cluster.example.kube_config_raw
+}
+
+output "client_key" {
+  value = azurerm_kubernetes_cluster.example.kube_config.0.client_key
+}
+
+output "client_certificate" {
+  value = azurerm_kubernetes_cluster.example.kube_config.0.client_certificate
+}
+
+output "cluster_ca_certificate" {
+  value = azurerm_kubernetes_cluster.example.kube_config.0.cluster_ca_certificate
+}
+
+output "host" {
+  value = azurerm_kubernetes_cluster.example.kube_config.0.host
+}
